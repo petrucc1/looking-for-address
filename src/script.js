f01602632c347de33a41df054b18c0043ffbb227
Sarah Petrucci <83620260+petrucc1@users.noreply.github.com>
2022-01-18 03:38:58 -0300

    Adding folders

---
+++ src/script.js
@@ -0,0 +1,44 @@
+"use strict"
+
+const cleanForm = (endereco) => {
+    document.getElementById('endereco').value = '';
+    document.getElementById('bairro').value = '';
+    document.getElementById('cidade').value = '';
+    document.getElementById('estado').value = '';
+}
+
+// Área de preenchimento
+const fillForm = (endereco) => {
+    document.getElementById('endereco').value = endereco.logradouro;
+    document.getElementById('bairro').value = endereco.bairro;
+    document.getElementById('cidade').value = endereco.localidade;
+    document.getElementById('estado').value = endereco.uf;
+}
+
+// Verificando se o CEP é válido
+const eNumero = (numero) => /^[0-9]+$/.test(numero); 
+
+const cepValido = (cep) => cep.length == 8 && eNumero(cep);
+
+// Pesquisando CEP digitado pelo usuário na API "ViaCEP"
+const pesquisarCep = async () => {
+    cleanForm();
+
+    const cep = document.getElementById('cep').value.replace("-", "");
+    const url = `https://viacep.com.br/ws/${cep}/json/`;
+    if (cepValido(cep)) {
+        const dados = await fetch(url);
+        const endereco = await dados.json();
+        if (endereco.hasOwnProperty('erro')) {
+            document.getElementById('endereco').value = 'CEP não encontrado!';
+        } else {
+            fillForm(endereco);
+        }
+    } else {
+        document.getElementById('endereco').value = 'CEP incorreto!';
+    }
+
+}
+
+document.getElementById('cep')
+    .addEventListener('focusout', pesquisarCep);
